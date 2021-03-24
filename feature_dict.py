83a15f02771d6db43289f8622ea6aad32c2a312e
Morgan-Sell <morganpsell@gmail.com>
2021-03-23 18:36:30 -0700

    Advanced processing code of visits dataset

---
+++ feature_dict.py
@@ -0,0 +1,12 @@
+import pandas as pandas
+import numpy as np
+git 
+
+visa_codes = {1: "Business",
+              2: "Pleasure",
+              3: "Student"}
+
+transport_mode = {1: "Air",
+                 2: "Sea",
+                 3: "Land",
+                 9: "Not reported"}
