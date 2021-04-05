07884a8d5035c718e1512e7076c9ca52321bcddd
Morgan-Sell <morganpsell@gmail.com>
2021-04-05 16:17:04 -0700

    uploaded files to S3. revising main_cloud.py

--- main_cloud.py
+++ main_cloud.py
@@ -1,12 +1,25 @@
-import configparser
 from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
+import os
+import configparser
 
 import config
 from etl import *
 
+
 config = configparser.Configparser()
-config.read("dl.cfg")
+config.read_file(open("dl.cfg"))
+
+os.environ["AWS_ACCESS_KEY_ID"] = config["AWS"]["AWS_ACCESS_KEY_ID"]
+os.environ["AWS_SECRET_ACCESS_KEY"] = config["AWS"]["AWS_SECRET_ACCESS_KEY"]
+
+DL_AIRPORT_DATA = config.get("S3", "AIRPORT_DATA")
+DL_USA_CITIES_DATA = config.get("S3", "USA_CITIES_DATA")
+DL_WEATHER_DATA = config.get("S3", "WEATHER_DATA")
+DL_TOURISM_DATE = config.get("S3", "TOURISM_DATA")
+DL_AIRPORT_CODES = config.get("S3", "AIRPORT_CODES")
+DL_COUNTRY_CODES = config.get("S3", "COUNTRY_CODES")
+
 
 def start_spark_session(session_app_name):
     """ 
@@ -24,10 +37,10 @@ def main():
     sc = start_spark_context(config.CONFIG_APP_NAME, config.CLUSTER_LOCATION)
     spark = start_spark_session(config.SESSION_APP_NAME)
 
-    process_airports_data(spark, config.AIRPORTS_URL_PATH, config.OUTPUT_PATH)
-    process_cities_demographics_data(spark, config.USA_CITIES_URL_PATH, config.OUTPUT_PATH)
-    process_usa_temperature_data(spark, config.TEMPERATURE_URL_PATH, config.OUTPUT_PATH)
-    process_usa_tourism_data(spark, config.TOURISM_URL_PATH, config.AIRPORT_CODES, config.COUNTRY_CODES, config.OUTPUT_PATH)
+    process_airports_data(spark, DL_AIRPORT_DATA, config.OUTPUT_PATH)
+    process_cities_demographics_data(spark, DL_USA_CITIES_DATA, config.OUTPUT_PATH)
+    process_usa_temperature_data(spark, DL_WEATHER_DATA, config.OUTPUT_PATH)
+    process_usa_tourism_data(spark, DL_TOURISM_DATA, DL_AIRPORT_CODES, DL_COUNTRY_CODES, config.OUTPUT_PATH)
 
 
     sc.stop()
